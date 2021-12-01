4e065e45b29e2edd1d1de2845aacda18d864a67a
Zeinab Gamal <zgamal@digitalroots.com.kw>
2021-12-01 23:45:04 +0200

    create link to make user go to login page in 404 not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {makeStyles} from '@mui/styles';
 import styles from '../assets/styles/main.js';
 import { Typography } from '@mui/material';
+//react router dom
+import {Link} from 'react-router-dom';
 
 
 const NotFound = () => {
@@ -14,6 +16,7 @@ const classes = useStyles();
         <div className={classes.container}>
                 <Typography variant="h1">404</Typography>
                 <Typography variant="h4">Sorry, but nothing exists here.</Typography>
+                <Link to="/" >Go to Log In Page</Link>
 
         </div>
     )
